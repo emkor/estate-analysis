e85624bd6b84ea8dec1b9a9567d4eb44fd545297
emkor93 <emkor93@gmail.com>
2020-06-20 11:03:35 +0200

    update creds with mapquest api key, add sourcing creds file in envrc for direnv tool

--- creds.sh
+++ creds.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export B2_KEY_ID="001a56c121eaece0000000009"
-export B2_APP_KEY="PASTE APP KEY HERE"
+export B2_APP_KEY="PLACEHOLDER"
 export B2_BUCKET="dl-estate"
+export MAPQUEST_API_KEY="PLACEHOLDER"
